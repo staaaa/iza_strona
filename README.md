0d8ca3665723b479a77bbcf753d92b0ce6b560a7
Stanisław Bożych <stachubozych@gmail.com>
2023-01-25 22:19:50 +0100

    config vite changed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# doradca
+# iza_strona
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/doradca",
+  base: "/iza_strona",
   plugins: [vue()],
   resolve: {
     alias: {
